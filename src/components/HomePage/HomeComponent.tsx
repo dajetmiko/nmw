103200999ef92ccb3482f4611db35ee545a8ccae
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-05-09 21:27:06 +0700

    contoh dogname

--- src/components/HomePage/HomeComponent.tsx
+++ src/components/HomePage/HomeComponent.tsx
@@ -1,30 +1,57 @@
-import { FC } from "react"
-import useCheckDarkMode from "@/utils/useCheckDarkMode"
-import NavbarComponent from "./NavbarComponent"
-import HeroComponent from "./HeroComponent"
-import ProdukComponent from "./ProdukComponent"
-import ServiceComponent from "./ServiceComponent"
-import PromoComponent from "./PromoComponent"
-import FooterComponent from "./FooterComponent"
+import { FC, useEffect, useState } from "react";
+import useCheckDarkMode from "@/utils/useCheckDarkMode";
+import NavbarComponent from "./NavbarComponent";
+import HeroComponent from "./HeroComponent";
+import ProdukComponent from "./ProdukComponent";
+import ServiceComponent from "./ServiceComponent";
+import PromoComponent from "./PromoComponent";
+import FooterComponent from "./FooterComponent";
+import { IResponseDog, delay } from "@/pages";
+import axios from "axios";
 
-const HomeComponent: FC<IHomeComponent> = ({}) => {
-  const [darkMode] = useCheckDarkMode()
+const HomeComponent: FC<IHomeComponent> = ({ data }) => {
+  const [darkMode] = useCheckDarkMode();
+  const [dataClient, setDataClient] = useState<null | IResponseDog>();
+  useEffect(() => {
+    const fetch = async () => {
+      await delay(2000);
+      const res = await axios.get<IResponseDog>(
+        "https://dog.ceo/api/breed/hound/list"
+      );
+      setDataClient(res.data);
+    };
+    fetch();
+  }, []);
   return (
-    <div className={`flex flex-col ${darkMode ? "bg-black" : "bg-white"} ${darkMode ? "text-white" : "text-black"}`}>
-      <NavbarComponent />
+    <div
+      className={`flex flex-col ${darkMode ? "bg-black" : "bg-white"} ${
+        darkMode ? "text-white" : "text-black"
+      }`}
+    >
+      <ul>
+        {dataClient?.message.map((dogName) => (
+          <li>{dogName + "CLIENT"}</li>
+        ))}
+      </ul>
+      <ul>
+        {data.message.map((dogName) => (
+          <li>{dogName}</li>
+        ))}
+      </ul>
+      {/* <NavbarComponent />
       <HeroComponent />
       <div className="ml-[12px] w-[calc(100% - 24px)] ">
         <ProdukComponent />
         <ServiceComponent />
       </div>
       <PromoComponent />
-      <FooterComponent />
+      <FooterComponent /> */}
     </div>
-  )
+  );
+};
+
+interface IHomeComponent {
+  data: IResponseDog;
 }
 
-interface IHomeComponent{
-
-}
-
-export default HomeComponent
+export default HomeComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import NavbarComponent from '@/components/HomePage/NavbarComponent'
-import { Provider } from 'react-redux';
-import store from '@/store';
-import HomeComponent from '@/components/HomePage/HomeComponent';
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import NavbarComponent from "@/components/HomePage/NavbarComponent";
+import { Provider } from "react-redux";
+import store from "@/store";
+import HomeComponent from "@/components/HomePage/HomeComponent";
+import axios from "axios";
+import { FC } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home: FC<IPropsHome> = ({ data }) => {
   return (
     <div>
       <Provider store={store}>
-        <HomeComponent />  
+        <HomeComponent data={data} />
       </Provider>
     </div>
-  )
+  );
+};
+
+interface IPropsHome {
+  data: IResponseDog;
 }
+
+export async function getServerSideProps() {
+  await delay(3000);
+  const res = await axios.get<IResponseDog>(
+    `https://dog.ceo/api/breed/terrier/list`
+  );
+  const data = res.data;
+
+  // Pass data to the page via props
+  return { props: { data } };
+}
+
+export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export interface IResponseDog {
+  message: string[];
+}
+
+export default Home;
